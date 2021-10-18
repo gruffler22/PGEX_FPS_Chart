7252e21ab70093daff249d2b9ccdd122991530ff
gruffler22 <jordanclarke22@hotmail.com>
2021-10-18 22:06:53 +0100

    Removed debug code - Whoops!

--- olcPGEX_Gruffler_FPS_Chart.h
+++ olcPGEX_Gruffler_FPS_Chart.h
@@ -1,19 +1,52 @@
 /*
-	+-------------------------------------------------------------+
-	|         OneLoneCoder Pixel Game Engine Extension            |
-	+-------------------------------------------------------------+
-	
+
+	PGEX Extension for OLC Pixel Game Engine
+
 	What is this?
-	~~~~~~~~~~~~~
-	This is an extension to the olcPixelGameEngine, which provides
-	the ability to see via a chart overlay the FPS performance on a frame
-	by frame and second by second basis.
+
+	This is an extension to the olcPixelGameEngine, which provides the ability to see via a chart overlay the FPS performance on a frame by frame and second by second basis.
 	This was made to to see if I was getting FPS spikes.
 
-	The licence is inherited from the OneLoneCoder Pixel Game Engine.
+	How to use?
+
+	Like any other PGEX extension you include the file in your project and include the line:
+
+	  #define OLC_PGEX_Gruffler_FPS_Chart_H
+
+	-once and only once in a .cpp file. 
+
+	Usually this is done in a .cpp file just after including the olcPixelGameEngine itself.
+	Eg.
+	  #define OLC_PGE_APPLICATION
+	  #include "olcPixelGameEngine.h"
+
+	  #define OLC_PGEX_Gruffler_FPS_Chart_H
+	  #include "olcPGEX_Gruffler_FPS_Chart.h"
+
+	To use the extension create an instance of the FPSChart class.
+	It only has two exposed members:
+
+		//Set whether to show FPS overlay chart or not
+		void showChart(bool show)
+
+		//Get whether the FPS chart is overlaying or not
+		bool isChartShowing() const
+
+	It all should just work in the background (via the olcPixelGameEngine).
+
+	The chart itself rescales dynamically on the last minutes worth of data.
+	The individual frames are plotted realtime in the foreground.
+	The seconds are summarised as candles in the background.
+
+	Made for my own uses but maybe someone else will find it useful!
+
+	Enjoy!
+
+
 
 	License (OLC-3)
-	~~~~~~~~~~~~~~~
+	(The licence is inherited from the OneLoneCoder Pixel Game Engine.)
+
 	Copyright 2018, 2019, 2020, 2021 OneLoneCoder.com
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions
@@ -38,7 +71,6 @@
 	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 */
 
 #pragma once
@@ -81,11 +113,13 @@ namespace olc
 		FPSChart(const FPSChart&) = delete;
 		FPSChart& operator = (FPSChart&) = delete;
 
+		//Set whether to show FPS overlay chart or not
 		void showChart(bool show)
 		{
 			_show = show;
 		}
 
+		//Get whether the FPS chart is overlaying or not
 		bool isChartShowing() const
 		{
 			return _show;
@@ -105,10 +139,6 @@ namespace olc
 			float out = 0.0f;
 			float high = 0.0f;
 			float low = 0.0f;
-			void debugPrint()
-			{
-				printf("In: %f Out: %f High: %f Low: %f\n", in, out, high, low);
-			}
 		};
 
 		interval _seconds[60];
@@ -159,7 +189,6 @@ namespace olc
 				_secSum -= 1.0f;
 				_wipInterval.out = currentFPS;
 				_seconds[_secsIter] = _wipInterval;
-				_seconds[_secsIter].debugPrint();
 				_secsIter++;
 				if (_secsIter >= 60)
 					_secsIter = 0;
